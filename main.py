bb8673725eed1a32c84c86e03569eef61b0b3f48
Jo Ye <jdy15@ic.ac.uk>
2023-05-05 11:32:54 +0100

    Changed number of epochs to 1000 for run3.

--- main.py
+++ main.py
@@ -42,7 +42,7 @@ parser.add_argument(
     "--pretrained_model_name", default="UNETR_model_best_acc.pth", type=str, help="pretrained model name"
 )
 parser.add_argument("--save_checkpoint", action="store_false", help="save checkpoint during training")
-parser.add_argument("--max_epochs", default=500, type=int, help="max number of training epochs")
+parser.add_argument("--max_epochs", default=1000, type=int, help="max number of training epochs")
 parser.add_argument("--batch_size", default=1, type=int, help="number of batch size")
 parser.add_argument("--sw_batch_size", default=1, type=int, help="number of sliding window batch size")
 parser.add_argument("--optim_lr", default=1e-4, type=float, help="optimization learning rate")
