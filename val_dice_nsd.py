33168b0e7833b811148186a0de8f2c954d9a549c
Jo Ye <jdy15@ic.ac.uk>
2023-05-06 10:01:41 +0100

    Test nsd calculation in val_dice_nsd.py with CT loader instead of MRI.

--- val_dice_nsd.py
+++ val_dice_nsd.py
@@ -141,12 +141,12 @@ def main():
 
     with torch.no_grad():
         # mean_dice_ct, mean_dice_per_organ_ct = calculate_score("dice", args, model, loader_ct)
-        # mean_nsd_ct, mean_nsd_per_organ_ct = calculate_score("nsd", args, model, loader_ct)
+        mean_nsd_ct, mean_nsd_per_organ_ct = calculate_score("nsd", args, model, loader_ct)
         
         # mean_dice_mri, mean_dice_per_organ_mri = calculate_score("dice", args, model, loader_mri)
-        mean_nsd_mri, mean_nsd_per_organ_mri = calculate_score("nsd", args, model, loader_mri)
+        # mean_nsd_mri, mean_nsd_per_organ_mri = calculate_score("nsd", args, model, loader_mri)
 
-        print(mean_nsd_per_organ_mri)
+        print(mean_nsd_per_organ_ct)
 
 
 if __name__ == "__main__":
