aebedcea8740d2e0328ff81b4d644efd7f128656
Jo Ye <jdy15@ic.ac.uk>
2023-05-15 08:21:57 +0100

    Change args to run 7b in val_dice_nsd.py.

--- val_dice_nsd.py
+++ val_dice_nsd.py
@@ -23,7 +23,7 @@ from monai.metrics import compute_surface_dice
 
 parser = argparse.ArgumentParser(description="UNETR segmentation pipeline")
 parser.add_argument(
-    "--pretrained_dir", default="./runs/run6b/", type=str, help="pretrained checkpoint directory"
+    "--pretrained_dir", default="./runs/run7b/", type=str, help="pretrained checkpoint directory"
 )
 parser.add_argument("--data_dir", default="./amos22/", type=str, help="dataset directory")
 parser.add_argument("--json_list", default="dataset_internal_val.json", type=str, help="dataset json file")
@@ -65,7 +65,7 @@ parser.add_argument("--lower", default=30.0, type=float, help="lower percentile 
 parser.add_argument("--upper", default=99.0, type=float, help="upper percentile in ScaleIntensityRangePercentilesd")
 parser.add_argument("--train_samples", default=40, type=int, help="number of samples per training image")
 parser.add_argument("--val_samples", default=20, type=int, help="number of samples per validation image")
-parser.add_argument("--train_sampling", default="unbalanced", type=str, help="sampling distribution of organs during training")
+parser.add_argument("--train_sampling", default="uniform", type=str, help="sampling distribution of organs during training")
 
 nsd_thresholds_mm = {
     1: 3,
