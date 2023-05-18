6e9317195523b3d18b13c3cefb929617b3ada6a0
Jo Ye <jdy15@ic.ac.uk>
2023-05-18 17:59:09 +0100

    Args for run16.

--- main.py
+++ main.py
@@ -34,7 +34,7 @@ from monai.utils.enums import MetricReduction
 
 parser = argparse.ArgumentParser(description="UNETR segmentation pipeline")
 parser.add_argument("--checkpoint", default=None, help="start training from saved checkpoint")
-parser.add_argument("--logdir", default="run15", type=str, help="directory to save the tensorboard logs")
+parser.add_argument("--logdir", default="run16", type=str, help="directory to save the tensorboard logs")
 parser.add_argument(
     "--pretrained_dir", default="./runs/run13/", type=str, help="pretrained checkpoint directory"
 )
@@ -98,7 +98,7 @@ parser.add_argument("--upper", default=99.0, type=float, help="upper percentile 
 parser.add_argument("--train_samples", default=40, type=int, help="number of samples per training image")
 parser.add_argument("--val_samples", default=20, type=int, help="number of samples per validation image")
 parser.add_argument("--train_sampling", default="uniform", type=str, help="sampling distribution of organs during training")
-parser.add_argument("--preprocessing", default=2, type=int, help="preprocessing option")
+parser.add_argument("--preprocessing", default=3, type=int, help="preprocessing option")
 parser.add_argument("--data_augmentation", action="store_false", help="use data augmentation during training")
 
 
