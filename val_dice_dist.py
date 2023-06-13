79907b4456b58dbc820a0d3c14298ed36fa93eaf
Jo Ye <jdy15@ic.ac.uk>
2023-06-13 09:04:09 +0100

    Change args to modality run1d in val_dice_dist.py.

--- val_dice_dist.py
+++ val_dice_dist.py
@@ -27,7 +27,7 @@ from monai.utils.misc import set_determinism
 
 parser = argparse.ArgumentParser(description="UNETR segmentation pipeline")
 parser.add_argument(
-    "--pretrained_dir", default="./runs_modality/run1/", type=str, help="pretrained checkpoint directory"
+    "--pretrained_dir", default="./runs_modality/run1d/", type=str, help="pretrained checkpoint directory"
 )
 parser.add_argument("--data_dir", default="./amos22/", type=str, help="dataset directory")
 parser.add_argument("--json_list", default="dataset_internal_val.json", type=str, help="dataset json file")
