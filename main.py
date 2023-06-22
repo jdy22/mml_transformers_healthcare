cf359417f43c519f3dc4d1dc85129f9863818a04
Jo Ye <jdy15@ic.ac.uk>
2023-06-22 10:43:17 +0100

    Args to resume training from checkpoint for modality run3.

--- main.py
+++ main.py
@@ -35,10 +35,10 @@ from monai.transforms import Activations, AsDiscrete, Compose
 from monai.utils.enums import MetricReduction
 
 parser = argparse.ArgumentParser(description="UNETR segmentation pipeline")
-parser.add_argument("--checkpoint", default=None, help="start training from saved checkpoint")
-parser.add_argument("--logdir", default="run3", type=str, help="directory to save the tensorboard logs")
+parser.add_argument("--checkpoint", default="./runs_modality/run3/model.pt", help="start training from saved checkpoint")
+parser.add_argument("--logdir", default="run3b", type=str, help="directory to save the tensorboard logs")
 parser.add_argument(
-    "--pretrained_dir", default=None, type=str, help="pretrained checkpoint directory"
+    "--pretrained_dir", default="./runs_modality/run3/", type=str, help="pretrained checkpoint directory"
 )
 parser.add_argument("--data_dir", default="./amos22/", type=str, help="dataset directory")
 parser.add_argument("--json_list", default="dataset_internal_val.json", type=str, help="dataset json file")
@@ -91,7 +91,7 @@ parser.add_argument("--dropout_rate", default=0.0, type=float, help="dropout rat
 parser.add_argument("--infer_overlap", default=0.5, type=float, help="sliding window inference overlap")
 parser.add_argument("--lrschedule", default="warmup_cosine", type=str, help="type of learning rate scheduler")
 parser.add_argument("--warmup_epochs", default=50, type=int, help="number of warmup epochs")
-parser.add_argument("--resume_ckpt", action="store_true", help="resume training from pretrained checkpoint")
+parser.add_argument("--resume_ckpt", action="store_false", help="resume training from pretrained checkpoint")
 parser.add_argument("--resume_jit", action="store_true", help="resume training from pretrained torchscript checkpoint")
 parser.add_argument("--smooth_dr", default=1e-6, type=float, help="constant added to dice denominator to avoid nan")
 parser.add_argument("--smooth_nr", default=0.0, type=float, help="constant added to dice numerator to avoid zero")
@@ -238,7 +238,7 @@ def main_worker(gpu, args):
             new_state_dict[k.replace("backbone.", "")] = v
         model.load_state_dict(new_state_dict, strict=False)
         if "epoch" in checkpoint:
-            start_epoch = checkpoint["epoch"]
+            start_epoch = checkpoint["epoch"] + 1
         if "best_acc" in checkpoint:
             best_acc = checkpoint["best_acc"]
         print("=> loaded checkpoint '{}' (epoch {}) (bestacc {})".format(args.checkpoint, start_epoch, best_acc))
