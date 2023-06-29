faa5a0fbf42f9788059823971bfcf3b2cea60dc0
Jo Ye <jdy15@ic.ac.uk>
2023-06-29 17:42:49 +0100

    Change args to organ run5 in val_classification.py.

--- post_processing/val_classification.py
+++ post_processing/val_classification.py
@@ -24,7 +24,7 @@ from monai.utils.misc import set_determinism
 
 parser = argparse.ArgumentParser(description="UNETR segmentation pipeline")
 parser.add_argument(
-    "--pretrained_dir", default="./runs_organ/run2/", type=str, help="pretrained checkpoint directory"
+    "--pretrained_dir", default="./runs_organ/run5/", type=str, help="pretrained checkpoint directory"
 )
 parser.add_argument("--data_dir", default="./amos22/", type=str, help="dataset directory")
 parser.add_argument("--json_list", default="dataset_internal_val.json", type=str, help="dataset json file")
@@ -72,7 +72,7 @@ parser.add_argument("--train_sampling", default="uniform", type=str, help="sampl
 parser.add_argument("--preprocessing", default=2, type=int, help="preprocessing option")
 parser.add_argument("--data_augmentation", action="store_false", help="use data augmentation during training")
 parser.add_argument("--additional_information", default="organ_classif", help="additional information provided to segmentation model")
-parser.add_argument("--classification_layer", default=12, type=int, help="Transformer layer for classification")
+parser.add_argument("--classification_layer", default=6, type=int, help="Transformer layer for classification")
 
 
 def calculate_accuracy(args, model, loader):
