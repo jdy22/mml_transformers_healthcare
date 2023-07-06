b922898617dc8150ed875a657c95d1f8cd180e6f
Jo Ye <jdy15@ic.ac.uk>
2023-07-06 11:09:40 +0100

    Test to obtain prompt embeddings from CLIP model.

--- data_utils/get_clip_embeddings.py
+++ data_utils/get_clip_embeddings.py
@@ -2,16 +2,17 @@ import torch
 import clip
 
 device = "cuda" if torch.cuda.is_available() else "cpu"
-model, preprocess = clip.load("ViT-B/32", device=device)
+model, preprocess = clip.load("ViT-L/14", device=device)
 
 prompts = [
     "a computerized tomography",
-    "a magnetic resonance image"
+    "a magnetic resonance image",
+    "a computerized tomography of a spleen",
+    "a magnetic resonance image of a spleen",
            ]
 
 text_tokens = clip.tokenize(prompts).to(device)
 with torch.no_grad():
     text_embeddings = model.encode_text(text_tokens)
 
-print(type(text_embeddings))
 print(text_embeddings.shape)
