2658ddee44e159edb5061cfdb3edfba470b018d3
Jo Ye <jdy15@ic.ac.uk>
2023-07-09 17:21:55 +0100

    Change args to organ run8 in val_dice_dist.py.

--- post_processing/read_logs.py
+++ post_processing/read_logs.py
@@ -1,7 +1,7 @@
 from tensorboard.backend.event_processing.event_accumulator import EventAccumulator
 import matplotlib.pyplot as plt
 
-logfile = "/Users/joannaye/Documents/_Imperial_AI_MSc/1_Individual_project/Code/individual_project/runs_organ/run11/events.out.tfevents.1688556589.gpu29.doc.ic.ac.uk"
+logfile = "/Users/joannaye/Documents/_Imperial_AI_MSc/1_Individual_project/Code/individual_project/runs_organ/run8/events.out.tfevents.1688477922.gpu28.doc.ic.ac.uk"
 
 event_acc = EventAccumulator(logfile)
 event_acc.Reload()
