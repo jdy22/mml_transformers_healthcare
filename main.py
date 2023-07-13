e6543171830728e0cdf91151900bc802f870bf42
Jo Ye <jdy15@ic.ac.uk>
2023-07-13 15:30:44 +0100

    Args to resume training from checkpoint for organ run12.

--- main.py
+++ main.py
@@ -36,10 +36,10 @@ from monai.transforms import Activations, AsDiscrete, Compose
 from monai.utils.enums import MetricReduction
 
 parser = argparse.ArgumentParser(description="UNETR segmentation pipeline")
-parser.add_argument("--checkpoint", default=None, help="start training from saved checkpoint")
-parser.add_argument("--logdir", default="run1-2", type=str, help="directory to save the tensorboard logs")
+parser.add_argument("--checkpoint", default="./runs_organ/run12/model.pt", help="start training from saved checkpoint")
+parser.add_argument("--logdir", default="run12b", type=str, help="directory to save the tensorboard logs")
 parser.add_argument(
-    "--pretrained_dir", default=None, type=str, help="pretrained checkpoint directory"
+    "--pretrained_dir", default="./runs_organ/run12/", type=str, help="pretrained checkpoint directory"
 )
 parser.add_argument("--data_dir", default="./amos22/", type=str, help="dataset directory")
 parser.add_argument("--json_list", default="dataset_internal_val.json", type=str, help="dataset json file")
@@ -92,7 +92,7 @@ parser.add_argument("--dropout_rate", default=0.0, type=float, help="dropout rat
 parser.add_argument("--infer_overlap", default=0.5, type=float, help="sliding window inference overlap")
 parser.add_argument("--lrschedule", default="warmup_cosine", type=str, help="type of learning rate scheduler")
 parser.add_argument("--warmup_epochs", default=50, type=int, help="number of warmup epochs")
-parser.add_argument("--resume_ckpt", action="store_true", help="resume training from pretrained checkpoint")
+parser.add_argument("--resume_ckpt", action="store_false", help="resume training from pretrained checkpoint")
 parser.add_argument("--resume_jit", action="store_true", help="resume training from pretrained torchscript checkpoint")
 parser.add_argument("--smooth_dr", default=1e-6, type=float, help="constant added to dice denominator to avoid nan")
 parser.add_argument("--smooth_nr", default=0.0, type=float, help="constant added to dice numerator to avoid zero")
@@ -103,7 +103,7 @@ parser.add_argument("--val_samples", default=20, type=int, help="number of sampl
 parser.add_argument("--train_sampling", default="uniform", type=str, help="sampling distribution of organs during training")
 parser.add_argument("--preprocessing", default=2, type=int, help="preprocessing option")
 parser.add_argument("--data_augmentation", action="store_false", help="use data augmentation during training")
-parser.add_argument("--additional_information", default="clip_early", help="additional information provided to segmentation model")
+parser.add_argument("--additional_information", default="organ_classif_inter3", help="additional information provided to segmentation model")
 parser.add_argument("--loss_combination_factor", default=1.0, type=float, help="combination factor for segmentation and classification losses")
 parser.add_argument("--classification_layer", default=12, type=int, help="Transformer layer for classification")
 
