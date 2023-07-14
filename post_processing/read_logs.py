a02226a770ee5a19bc848def8ddaf58d68ec06ae
Jo Ye <jdy15@ic.ac.uk>
2023-07-14 16:38:48 +0100

    Change args to clip run1 in val_dice_dist.py.

--- post_processing/read_logs.py
+++ post_processing/read_logs.py
@@ -1,7 +1,7 @@
 from tensorboard.backend.event_processing.event_accumulator import EventAccumulator
 import matplotlib.pyplot as plt
 
-logfile = "/Users/joannaye/Documents/_Imperial_AI_MSc/1_Individual_project/Code/individual_project/runs_clip/run1-2/events.out.tfevents.1689238562.gpu09.doc.ic.ac.uk"
+logfile = "/Users/joannaye/Documents/_Imperial_AI_MSc/1_Individual_project/Code/individual_project/runs_clip/run1/events.out.tfevents.1689095260.gpu25.doc.ic.ac.uk"
 
 event_acc = EventAccumulator(logfile)
 event_acc.Reload()
