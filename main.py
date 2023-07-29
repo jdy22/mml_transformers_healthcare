6d77c0d0369781037aa2d74ad81b084ce0c61da2
Jo Ye <jdy15@ic.ac.uk>
2023-07-29 09:50:21 +0100

    Set args for third run of clip run1.

--- main.py
+++ main.py
@@ -37,7 +37,7 @@ from monai.utils.enums import MetricReduction
 
 parser = argparse.ArgumentParser(description="UNETR segmentation pipeline")
 parser.add_argument("--checkpoint", default=None, help="start training from saved checkpoint")
-parser.add_argument("--logdir", default="run6-3", type=str, help="directory to save the tensorboard logs")
+parser.add_argument("--logdir", default="run1-3", type=str, help="directory to save the tensorboard logs")
 parser.add_argument(
     "--pretrained_dir", default=None, type=str, help="pretrained checkpoint directory"
 )
@@ -103,7 +103,7 @@ parser.add_argument("--val_samples", default=20, type=int, help="number of sampl
 parser.add_argument("--train_sampling", default="uniform", type=str, help="sampling distribution of organs during training")
 parser.add_argument("--preprocessing", default=2, type=int, help="preprocessing option")
 parser.add_argument("--data_augmentation", action="store_false", help="use data augmentation during training")
-parser.add_argument("--additional_information", default="organ_late", help="additional information provided to segmentation model")
+parser.add_argument("--additional_information", default="clip_early", help="additional information provided to segmentation model")
 parser.add_argument("--loss_combination_factor", default=1.0, type=float, help="combination factor for segmentation and classification losses")
 parser.add_argument("--classification_layer", default=12, type=int, help="Transformer layer for classification")
 
