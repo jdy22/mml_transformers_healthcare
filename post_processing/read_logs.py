2b58863b0a4843efff669c3d97179461e5033d3d
Jo Ye <jdy15@ic.ac.uk>
2023-08-10 10:59:47 +0100

    Change args to second run of clip_run2 in val_dice_dist.py.

--- post_processing/read_logs.py
+++ post_processing/read_logs.py
@@ -1,7 +1,7 @@
 from tensorboard.backend.event_processing.event_accumulator import EventAccumulator
 import matplotlib.pyplot as plt
 
-logfile = "/Users/joannaye/Documents/_Imperial_AI_MSc/1_Individual_project/Code/individual_project/runs_organ/run9-3/events.out.tfevents.1690620173.gpu04.doc.ic.ac.uk"
+logfile = "/Users/joannaye/Documents/_Imperial_AI_MSc/1_Individual_project/Code/individual_project/runs_clip/run2-2/events.out.tfevents.1690290642.gpu05.doc.ic.ac.uk"
 
 event_acc = EventAccumulator(logfile)
 event_acc.Reload()
