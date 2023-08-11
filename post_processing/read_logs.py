28e61f74bf154644a861bf49e73b744ff5443a70
Jo Ye <jdy15@ic.ac.uk>
2023-08-11 09:22:03 +0100

    Args to resume training from checkpoint for third run of organ_run8.

--- post_processing/read_logs.py
+++ post_processing/read_logs.py
@@ -1,7 +1,7 @@
 from tensorboard.backend.event_processing.event_accumulator import EventAccumulator
 import matplotlib.pyplot as plt
 
-logfile = "/Users/joannaye/Documents/_Imperial_AI_MSc/1_Individual_project/Code/individual_project/runs_clip/run2-3/events.out.tfevents.1690621078.gpu28.doc.ic.ac.uk"
+logfile = "/Users/joannaye/Documents/_Imperial_AI_MSc/1_Individual_project/Code/individual_project/runs_organ/run8-3b/events.out.tfevents.1691658636.gpu01.doc.ic.ac.uk"
 
 event_acc = EventAccumulator(logfile)
 event_acc.Reload()
